ab448f72bd1bad8a8dfb97016681e8e9a3b067d2
Jeffrey Lee <darkthread@gmail.com>
2021-07-16 16:18:28 +0800

    程式微調

--- src/DemoWeb/Reports/DemoReport.aspx.cs
+++ src/DemoWeb/Reports/DemoReport.aspx.cs
@@ -15,15 +15,15 @@ namespace DemoWeb.Reports
 {
     public partial class DemoReport : System.Web.UI.Page
     {
-
-
         protected void Page_Load(object sender, EventArgs e)
         {
+            //https://blog.darkthread.net/blog/report-viewer-infinite-loop/
+            //防止無窮迴圈
             if (ScriptManager1.IsInAsyncPostBack) return;
 
-            var st = Request["st"] ?? string.Empty;
-            var ed = Request["ed"] ?? string.Empty;
-            var s = Request["s"] ?? string.Empty;
+            var st = Request.Form["st"] ?? Request.QueryString["st"] ?? string.Empty;
+            var ed = Request.Form["ed"] ?? Request.QueryString["ed"] ?? string.Empty;
+            var s = Request.Form["s"] ?? Request.QueryString["s"] ?? string.Empty;
             var p = Request["p"] ?? "NA";
             var paramDict = (MemoryCache.Default.Get(p)) as Dictionary<string, string>;
             if (paramDict != null)
@@ -34,6 +34,7 @@ namespace DemoWeb.Reports
             }
             if (string.IsNullOrEmpty(st) || string.IsNullOrEmpty(ed) || string.IsNullOrEmpty(s))
             {
+                //未傳參數時顯示空白
                 ReportViewer1.Visible = false;
             }
             else
@@ -42,10 +43,13 @@ namespace DemoWeb.Reports
                 ReportViewer1.ProcessingMode = ProcessingMode.Local;
                 ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/Reports/PlayerReport.rdlc");
 
-                //此處理 DataView 模擬查詢資料庫
+                //此處以 DataView 模擬查詢資料庫
                 DataView view = SimulateData.DataTable.DefaultView;
+                //指定排序依據
                 view.Sort = s;
-                view.RowFilter = $"RegDate >= '{DateTime.Parse(st):yyyy-MM-dd}' AND RegDate <= '{DateTime.Parse(ed):yyyy-MM-dd}'";
+                //指定查詢區間
+                view.RowFilter = 
+                    $"RegDate >= '{DateTime.Parse(st):yyyy-MM-dd}' AND RegDate <= '{DateTime.Parse(ed):yyyy-MM-dd}'";
 
                 ReportDataSource ds = new ReportDataSource("DataSet1", view.ToTable());
                 ReportViewer1.LocalReport.DataSources.Clear();
@@ -55,7 +59,5 @@ namespace DemoWeb.Reports
                 ReportViewer1.LocalReport.SetParameters(new ReportParameter("SortBy", s));
             }
         }
-
-
     }
 }
