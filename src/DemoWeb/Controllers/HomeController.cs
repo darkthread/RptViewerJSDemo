3866c43c3ba0e3fcb33cde52786b2f770da3ead4
Jeffrey Lee <darkthread@gmail.com>
2021-07-16 14:12:17 +0800

    ReportViewer for MVC 展示

--- src/DemoWeb/Controllers/HomeController.cs
+++ src/DemoWeb/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DemoWeb.Models;
+using Microsoft.Reporting.WebForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,23 @@ namespace DemoWeb.Controllers
         public ActionResult Index()
         {
             return Redirect("~/SPA/report.html");
+        }
+
+        public ActionResult RptViewerInMvc()
+        {
+            var rv = new ReportViewer();
+            rv.ProcessingMode = ProcessingMode.Local;
+            rv.LocalReport.ReportPath =
+                Server.MapPath("~/Reports/PlayerReport.rdlc");
+            ReportDataSource ds = new ReportDataSource("DataSet1",
+                SimulateData.DataTable);
+            rv.LocalReport.DataSources.Clear();
+            rv.LocalReport.DataSources.Add(ds);
+            rv.LocalReport.SetParameters(new ReportParameter("StDate", "2000-01-01"));
+            rv.LocalReport.SetParameters(new ReportParameter("EdDate", "2000-01-01"));
+            rv.LocalReport.SetParameters(new ReportParameter("SortBy", "PlayerId"));
+            ViewBag.ReportViewer = rv;
+            return View();
         }
     }
 }
